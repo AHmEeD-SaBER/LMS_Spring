48614e0f1df179d3046e5d7cc2e847324e7477f5
AHmEeD-SaBER <ahmedsaber3620004@gmail.com>
2024-12-13 16:45:47 +0200

    Uploading Files

---
+++ src/test/java/dev/FCAI/LMS_Spring/LmsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.FCAI.LMS_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
